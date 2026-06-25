615695e1cdc1c35d87f622492798f03d19db2c83
Bung Khoai <bungkhoai05@gmail.com>
2026-06-25 09:28:56 +0700

    Hệ thống di chuyển rắn hoàn chỉnh

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Vector3 direction = Vector3.right;
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+    && direction != Vector3.down)
+        {
+            direction = Vector3.up;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            && direction != Vector3.up)
+        {
+            direction = Vector3.down;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            && direction != Vector3.right)
+        {
+            direction = Vector3.left;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            && direction != Vector3.left)
+        {
+            direction = Vector3.right;
+        }
+
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
